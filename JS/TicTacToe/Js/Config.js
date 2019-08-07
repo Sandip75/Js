ee1922bc4ab728fbac5ef3276b83f0296c6767d9
Sandip75 <sk1957339@gmail.com>
2019-08-07 22:40:28 +0530

    js

--- JS/TicTacToe/Js/Config.js
+++ JS/TicTacToe/Js/Config.js
@@ -1,24 +1,10 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyCtQIwBfsic5vgpPHpU5idrouIrUI6kptM",
-  authDomain: "login-2dc47.firebaseapp.com",
-  databaseURL: "https://login-2dc47.firebaseio.com",
-  projectId: "login-2dc47",
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
   storageBucket: "",
-  messagingSenderId: "62319529308",
-  appId: "1:62319529308:web:907f6d9204356fa9"
+  messagingSenderId: ,
+  appId: 
 };
 firebase.initializeApp(firebaseConfig);
-
-
-  // Your web app's Firebase configuration
-  // var firebaseConfig = {
-  //   apiKey: "AIzaSyCUdLq3KmMwkGn4QByoGL2sHRZ5nQ1qz1o",
-  //   authDomain: "tictactoe-ad886.firebaseapp.com",
-  //   databaseURL: "https://tictactoe-ad886.firebaseio.com/",
-  //   projectId: "tictactoe-ad886",
-  //   storageBucket: "",
-  //   messagingSenderId: "687358867770",
-  //   appId: "1:687358867770:web:a9234270c6472d53"
-  // };
-  // // Initialize Firebase
-  // firebase.initializeApp(firebaseConfig);

9c1c5e689237ebcfaa38e2ea7a38f81d6182b6b9
Sandip75 <sk1957339@gmail.com>
2019-08-08 07:13:29 +0530

    js

--- JS/ToDoList/Js/controllers/taskCtrl.js
+++ JS/ToDoList/Js/controllers/taskCtrl.js
@@ -1,6 +1,66 @@
-alert("Hello");
-window.addEventListener('iAdd' , add);
+window.addEventListener("click" , bindEvents);
+
+function bindEvents(){
+    document.getElementById('iAdd').addEventListener('click' , add);
+}
 
 function add(){
-    alert("Hiiii");
+    let id = document.getElementById("iId").value;
+    let name = document.getElementById("iName").value;
+    let description = document.getElementById("iDescription").value;
+    let endDate = document.getElementById("iEndDate").value;
+    taskOperation.AddToDo(id,name,description,endDate);
+
+    // Reload the task in list 
+    var tasks = taskOperation.Read();
+    displayTasks(tasks);
+    clear();
+    //console.log(tasks);
 }
+
+function clear(){
+    document.getElementById("iId").value = "";
+    document.getElementById("iName").value = "";
+    document.getElementById("iDescription").value = "";
+    document.getElementById("iEndDate").value = "";    
+}
+
+function displayTasks(tasks){
+    for(let task of tasks){
+        printTask(task);
+    }
+}
+
+function printTask(task){
+    var ul = document.getElementById("otask");
+    var li = document.createElement('li');
+    // li.appendChild(addTaskId(task.id));
+    // li.appendChild(addTaskName(task.name));
+    // li.appendChild(addTaskDescription(task.description));
+    // li.appendChild(addTaskEndDate(task.date));
+
+    li.appendChild(createSpan(task.id , task.name , task.description,task.date));
+    ul.appendChild(li);
+}
+
+function createSpan(id , name , description , endDate){
+    var span = document.createElement('span');
+    span.innerText = id + " " + name + " " + description + " " + endDate ;
+    return span;
+}
+
+// function addTaskId(id){
+
+// }
+
+// function addTaskName(name){
+
+// }
+
+// function addTaskDescription(description){
+
+// }
+
+// function addTaskEndDate(endDate){
+
+// }
--- JS/ToDoList/Js/models/taskOperation.js
+++ JS/ToDoList/Js/models/taskOperation.js
@@ -1,8 +1,16 @@
 const taskOperation = {
     tasks : [] , 
 
-
     Add(){
 
+    },
+
+    Read(){
+        return this.tasks;
+    },
+
+    AddToDo(id , name , description , enddate){
+        let newTask = new Task(id,name,description,enddate);
+        this.tasks.push(newTask);
     }
 }
